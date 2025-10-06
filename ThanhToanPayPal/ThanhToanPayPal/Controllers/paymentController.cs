b922d486bdad85ef6f01f78dffb5bd77df1798d0
HIENLTH @ HCMUE <hyhien@gmail.com>
2025-10-06 15:51:52 +0700

    Change payload

--- ThanhToanPayPal/ThanhToanPayPal/Controllers/paymentController.cs
+++ ThanhToanPayPal/ThanhToanPayPal/Controllers/paymentController.cs
@@ -7,12 +7,13 @@ namespace ThanhToanPayPal.Controllers
     {
         private readonly PaypalClient _paypalClient;
 
-        public PaymentController(PaypalClient paypalClient) {
+        public PaymentController(PaypalClient paypalClient)
+        {
             _paypalClient = paypalClient;
         }
 
         [HttpPost("/payment/create-paypal-order")]
-        public async Task<IActionResult> CreateOrder()
+        public async Task<IActionResult> CreateOrder([FromBody] Amount amount)
         {
             //số tiền có thể lấy từ Session hoặc dưới client gửi lên
             var value = "1234";
@@ -24,17 +25,29 @@ namespace ThanhToanPayPal.Controllers
                 var response = await _paypalClient.CreateOrder(value, currency_code, refId);
                 return Ok(response);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                return BadRequest($"Lỗi: {ex.Message}");
+                var error = new { ex.GetBaseException().Message };
+                return BadRequest(error);
             }
         }
 
-        [HttpPost("/payment/capture-paypal-order/{orderID}")]
+        [HttpPost("/payment/capture-paypal-order")]
         public IActionResult CaptureOrder(string orderID)
         {
-            _paypalClient.CaptureOrder(orderID);
-            return View();
+            try
+            {
+                var response = _paypalClient.CaptureOrder(orderID);
+
+                //Lưu đơn hàng vô database
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                var error = new { ex.GetBaseException().Message };
+                return BadRequest(error);
+            }
         }
     }
 }
