0a17257428389596a8bec3870e753eb871a0e573
HIENLTH @ HCMUE <hyhien@gmail.com>
2025-10-06 14:28:20 +0700

    Design frontend with paypal button

---
+++ ThanhToanPayPal/ThanhToanPayPal/Controllers/paymentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThanhToanPayPal.Controllers
+{
+    public class PaymentController : Controller
+    {
+        [HttpPost("/payment/create-paypal-order")]
+        public IActionResult CreateOrder()
+        {
+            return View();
+        }
+
+        [HttpPost("/payment/capture-paypal-order")]
+        public IActionResult CaptureOrder()
+        {
+            return View();
+        }
+    }
+}
